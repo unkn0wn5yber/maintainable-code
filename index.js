487a9c0e7c76acb32826e1879a9d21c4d793bb46
tom684 <105120716+unkn0wn5yber@users.noreply.github.com>
2022-05-24 10:32:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function dirttool($path){
+  if(!is_dir($path)) {//checks to see if it is a directory
+exit("<br><strong>$path is NOT a directory</strong>");
+  }// end of if
+$this->path=$path;
+ $from ="";
+  $to ="";
+  //reads the directory
+  $slash =$slash();
+  $verz =opendir($path);
+  while ($file = readdir($verz)) {
+  if ()
+  }
+}
